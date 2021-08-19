719813eeb147ca4ecad89b52723d15c42f39fb82
emapall <ema.pallaoro@gmail.com>
2021-08-19 17:44:51 +0200

    added new kolibri folder

---
+++ submitted_models/asl_kolibri_sensor_config_1/launch/spawner.rb
@@ -0,0 +1,51 @@
+def spawner(_name, _modelURI, _worldName, _x, _y, _z, _roll, _pitch, _yaw)
+  <<-HEREDOC
+  <spawn name='#{_name}'>
+      <name>#{_name}</name>
+      <allow_renaming>false</allow_renaming>
+      <pose>#{_x} #{_y} #{_z + 0.2} #{_roll} #{_pitch} #{_yaw}</pose>
+      <world>#{_worldName}</world>
+      <is_performer>true</is_performer>
+      <sdf version='1.6'>
+      <include>
+      <name>#{_name}</name>
+      <uri>#{_modelURI}</uri>
+      <!-- Publish robot state information -->
+      <plugin filename="libignition-gazebo-pose-publisher-system.so"
+        name="ignition::gazebo::systems::PosePublisher">
+        <publish_link_pose>true</publish_link_pose>
+        <publish_sensor_pose>true</publish_sensor_pose>
+        <publish_collision_pose>false</publish_collision_pose>
+        <publish_visual_pose>false</publish_visual_pose>
+        <publish_nested_model_pose>false</publish_nested_model_pose>
+        <use_pose_vector_msg>true</use_pose_vector_msg>
+        <static_publisher>true</static_publisher>
+        <static_update_frequency>60</static_update_frequency>
+      </plugin>
+      <plugin filename="libignition-gazebo-pose-publisher-system.so"
+        name="ignition::gazebo::systems::PosePublisher">
+        <publish_link_pose>false</publish_link_pose>
+        <publish_sensor_pose>false</publish_sensor_pose>
+        <publish_collision_pose>false</publish_collision_pose>
+        <publish_visual_pose>false</publish_visual_pose>
+        <publish_nested_model_pose>#{$enableGroundTruth}</publish_nested_model_pose>
+        <use_pose_vector_msg>true</use_pose_vector_msg>
+        <static_publisher>false</static_publisher>
+      </plugin>
+      
+    </include>
+    </sdf>
+  </spawn>
+  HEREDOC
+end
+
+def rosExecutables(_name, _worldName)
+  <<-HEREDOC
+    <executable name='kolibri_description'>
+      <command>roslaunch --wait asl_kolibri_sensor_config_1 description.launch world_name:=#{_worldName} name:=#{_name}</command>
+    </executable>
+    <executable name='kolibri_ros_ign_bridge'>
+      <command>roslaunch --wait asl_kolibri_sensor_config_1 vehicle_topics.launch world_name:=#{_worldName} name:=#{_name} uav:=1 laser_scan:=0 top_scan:=1 bottom_scan:=1 rgbd_cam:=1    </command>
+  </executable>
+  HEREDOC
+end
